6c3cd8d66acebb6d200660b38dfb053664abe43f
Matthew B <matthewburgess.dev@gmail.com>
2017-11-08 11:42:34 +0000

    most recent updates

--- Start.php
+++ Start.php
@@ -2,8 +2,21 @@
 
 $bootConfig = (include __DIR__.'/Config.php');
 
+//required framework
+$defaultIncludes = [
+    __DIR__.'/Bootstraps/'.$bootConfig['bootstrapFile']
+];
+
+//preset tools
+foreach (glob("tools/*.php") as $filename){
+    $defaultIncludes[] = $filename;
+}
+
+//the whiteboard
+$defaultIncludes[] = __DIR__.'/Whiteboard.php';
+
 $config = new \Psy\Configuration();
-$config->setDefaultIncludes(array(__DIR__.'/Bootstraps/'.$bootConfig['bootstrapFile'], __DIR__.'/Whiteboard.php'));
+$config->setDefaultIncludes($defaultIncludes);
 
 $shell = new \Psy\Shell($config);
 $shell->run();
---
+++ tools/Asserts.php
@@ -0,0 +1,2 @@
+<?php
+class Assert extends Webmozart\Assert\Assert{}
