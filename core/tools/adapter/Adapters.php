8bc7deec0ac5eec5e7f29b20e62450320e22ec55
Matthew B <matthewburgess.dev@gmail.com>
2017-11-19 22:21:36 +0000

    further slim updates

--- core/tools/adapter/Adapters.php
+++ core/tools/adapter/Adapters.php
@@ -1,10 +1,15 @@
 <?php
-$adaptersLoader = require __DIR__ . '/../vendor/autoload.php';
-
-foreach (glob(__DIR__."/*.php") as $filename){
-    if(basename(__FILE__, '.php') !== basename($filename, '.php')){
-        require $filename;
+if(file_exists(__DIR__ . '/../vendor/autoload.php')){
+    $adaptersLoader = require_once __DIR__ . '/../vendor/autoload.php';
+    
+    foreach (glob(__DIR__."/*.php") as $filename){
+        if(basename(__FILE__, '.php') !== basename($filename, '.php')){
+            require $filename;
+        }
     }
+}
+else{
+   echo "No tools loaded.\n\n"; 
 }
 
 
